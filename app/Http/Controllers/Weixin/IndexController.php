24c0be0083c888ff19e1d151eeadb2b9cce7a769
mayatong <2593787142@qq.com>
2020-11-11 20:20:26 +0800

    获取素材

--- app/Http/Controllers/Weixin/IndexController.php
+++ app/Http/Controllers/Weixin/IndexController.php
@@ -258,12 +258,12 @@ class IndexController extends Controller
         echo $res;
     }
 
-    //获取素材
+    //素材下载
     public function media(){
         $xml = file_get_contents("php://input");
 //        file_put_contents('wx_event.log',$xml);die;
         $obj = simplexml_load_string($xml,'SimpleXMLElement',LIBXML_NOCDATA);
-        $media_id = $obj->FromUserName;
+        $media_id = $obj->MediaId;
         $access_token = $this->gettoken();
         $url = "https://api.weixin.qq.com/cgi-bin/media/get?access_token=".$access_token."&media_id=".$media_id;
         $res = file_get_contents($url);
